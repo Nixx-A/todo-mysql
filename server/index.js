76866263a11998e3ca6b184fe6b8d84905c4ceef
Nicolas Ayan <nicolas.ayan18'@'gmail.com>
2023-07-27 00:56:30 -0300

    Server deployment join with the build

--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ import express from 'express'
 import indexRoutes from "./routes/index.routes.js";
 import todosRoutes from "./routes/todos.routes.js";
 import cors from "cors";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 
 const app = express()
+const __dirname = dirname(fileURLToPath(import.meta.url));
+console.log(__dirname);
 
 
 app.use(cors())
-
 app.use(express.json())
+
 app.use(indexRoutes)
 app.use(todosRoutes)
 
+app.use(express.static(join(__dirname, "../client/dist")))
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!')
